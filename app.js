59a0c6c7424e53fe0fc49f2e69a1ad58b2ce11fe
Fabio Nascimento <34170458+fabionascimento1@users.noreply.github.com>
2022-02-09 16:29:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+
+const app = express();
+
+require("dotenv").config();
+
+const DATABASE = process.env.DATABASE;
+mongoose
+  .connect(DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+  })
+  .then(() => {
+    console.log("DATABASE connected!");
+  });
+
+const authRoutes = require("./src/routes/auth");
+const companyRoutes = require("./src/routes/company");
+const sportcourt = require("./src/routes/sportcourt");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/api", authRoutes);
+app.use("/api", companyRoutes);
+app.use("/api", sportcourt);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Run server at: http://localhost:${port}/`);
+});
